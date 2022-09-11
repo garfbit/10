c089d45f94adfc65e8fe934e49e028d21a06f545
garfbit <104676660+garfbit@users.noreply.github.com>
2022-09-11 10:10:27 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+let Tiempo = 0
+let Horas = 0
+let Minutos = 0
+let Segundos = 0
 basic.forever(function () {
-	
+    Tiempo = 99999
+    Horas = Tiempo / 3600
+    Tiempo = Tiempo % 3600
+    Minutos = Tiempo / 60
+    Segundos = Tiempo % 60
+    basic.showNumber(Horas)
+    basic.showNumber(Minutos)
+    basic.showNumber(Segundos)
 })
